2290d9e716bd61454bd54c26dc0fddcb8adf01f3
Aligo Kang <aligo_x@163.com>
2012-07-18 10:50:20 +0800

    i hate java

--- src/com/aligo/idle/contacts/ContactsList.java
+++ src/com/aligo/idle/contacts/ContactsList.java
@@ -1,7 +1,9 @@
 package com.aligo.idle.contacts;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.aligo.idle.R;
 
@@ -23,6 +25,8 @@ public class ContactsList extends Activity {
 
     private ContactsAdapter adapter;
     private ListView listView;
+    private List<Map<String, String>> contacts;
+    private HashMap<String, String> itemmap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,8 +37,9 @@ public class ContactsList extends Activity {
         listView.setAdapter(adapter);
     }
    
-    private List<String> getContacts() {
-        List<String> contacts_name = new ArrayList<String>();
+    private List<Map<String, String>> getContacts() {
+        contacts = new ArrayList<Map<String, String>>();
+        addMyself();
         Uri uri = ContactsContract.Contacts.CONTENT_URI;
         String[] projection = new String[] {
                 ContactsContract.Contacts._ID,
@@ -43,24 +48,49 @@ public class ContactsList extends Activity {
         String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
         Cursor cur =  managedQuery(uri, projection, null, null, sortOrder);
         if (cur.getCount() > 0) {
+        	itemmap = new HashMap<String, String>();
+            itemmap.put("name", cur.getCount() + " contacts");
+            itemmap.put("type", "header");
+            contacts.add(itemmap);
             while (cur.moveToNext()) {
                 String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                contacts_name.add(name);
+                itemmap = new HashMap<String, String>();
+                itemmap.put("name", name);
+                itemmap.put("type", "contact");
+                contacts.add(itemmap);
             }
         }
-        return contacts_name;
+        return contacts;
     }
     
-    private static class ContactsAdapter extends ArrayAdapter<String> {
-        public ContactsAdapter(Context context, List<String> objects) {
+    private void addMyself() {
+    	itemmap = new HashMap<String, String>();
+    	itemmap.put("name", "Myself");
+    	itemmap.put("type", "header");
+        contacts.add(itemmap);
+        itemmap = new HashMap<String, String>();
+        itemmap.put("name", "My name");
+        itemmap.put("type", "self");
+        contacts.add(itemmap);
+    }
+    
+    private static class ContactsAdapter extends ArrayAdapter<Map<String, String>> {
+        public ContactsAdapter(Context context, List<Map<String, String>> objects) {
             super(context, 0, objects);
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;             
-            view = LayoutInflater.from(getContext()).inflate(R.layout.contacts_list_item, null);
-            TextView textView = (TextView) view.findViewById(R.id.contact_name);
-            textView.setText(getItem(position));
+            View view = convertView;
+            Map<String, String> map = getItem(position);
+            if ( map.get("type") == "header") {
+            	view = LayoutInflater.from(getContext()).inflate(R.layout.contacts_list_item_header, null);
+                TextView textView = (TextView) view.findViewById(R.id.name);
+                textView.setText(getItem(position).get("name"));
+            } else {
+            	view = LayoutInflater.from(getContext()).inflate(R.layout.contacts_list_item, null);
+                TextView textView = (TextView) view.findViewById(R.id.name);
+                textView.setText(getItem(position).get("name"));
+            }
             return view;
         }
     }
